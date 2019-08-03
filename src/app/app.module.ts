4cfc781faf2091c5da62ebe9276c6a6906fd67e8
Collin Argo <collinargo@gmail.com>
2019-08-03 13:15:00 -0400

    add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    FormComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
