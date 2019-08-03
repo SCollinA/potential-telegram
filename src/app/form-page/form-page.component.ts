27845e6b6b100ecb1cc219743838e39b071f8b43
Collin Argo <collinargo@gmail.com>
2019-08-03 15:33:18 -0400

    remove modal from form-page

--- src/app/form-page/form-page.component.ts
+++ src/app/form-page/form-page.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { EventService } from '../event.service';
 
 @Component({
   selector: 'app-form-page',
   templateUrl: './form-page.component.html',
   styleUrls: ['./form-page.component.scss']
 })
-export class FormPageComponent implements OnInit {
+export class FormPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  componentId: string;
+  closeFormEventId: string;
+  eventSubscription: Subscription;
+
+  isModalOpen = false;
+
+  constructor(private eventService: EventService) {
+
+  }
 
   ngOnInit() {
+    this.componentId = this.eventService.registerComponent();
+    this.closeFormEventId = this.eventService.registerEvent(this.componentId);
+
+    this.eventSubscription = this.eventService.event.subscribe((event: any) => {
+      if (event.targetId === this.componentId) {
+        this.handleEvent(event);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.eventSubscription.unsubscribe();
+  }
+
+  private handleEvent(event: any): void {
+    console.log('event', event);
+    switch (event.eventId) {
+      case this.closeFormEventId: history.back();
+                                  break;
+      default: break;
+    }
   }
 
 }
