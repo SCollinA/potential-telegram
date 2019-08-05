7fd4a4c6cec357617b76e42e683b754f81d5d3d0
Collin Argo <collinargo@gmail.com>
2019-08-05 19:44:13 -0400

    refactor event service to only track events

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -7,65 +7,23 @@ import { Subject, Subscription } from 'rxjs';
 })
 export class EventService {
 
-  private componentMap = {};
   private eventMap = {};
-  private event = new Subject<any>();
-
-  event$ = this.event.asObservable();
 
   constructor() { }
 
-  // accept string for id, then child component can just use that string
-  registerComponent(): string {
-    const componentId = uuid();
-    this.componentMap[componentId] = {};
-    return componentId;
-  }
-
-  registerEvent(targetId: string, eventHandler: any): any {
-    const eventId = uuid();
-    this.eventMap[eventId] = targetId;
-    this.componentMap[targetId][eventId] = eventHandler;
-    this.componentMap[targetId].eventSubscription = this.event$.subscribe(this.handleEvent);
-    return eventId;
+  registerEvent(eventId: string): any {
+    this.eventMap[eventId] = new Subject<any>();
+    return this.eventMap[eventId];
   }
 
   triggerEvent(eventId: string): void {
-    const targetId = this.eventMap[eventId];
-    if (
-      !eventId ||
-      !targetId
-      ) { return undefined; }
-    this.event.next({
-      eventId,
-      targetId
-    });
+    const event$ = this.eventMap[eventId];
+    if (!eventId ) { return undefined; }
+    event$.next();
   }
 
-  handleEvent = (liveEvent: any): void => {
-    const events = this.componentMap[liveEvent.targetId];
-    if (typeof events[liveEvent.eventId] === "function") {
-      events[liveEvent.eventId]();
-    }
-  }
-
-  subscribeToEvent = (eventId: string, callback: any): Subscription => {
-    return this.event$.subscribe((event: any) => {
-      if (event.eventId === eventId) {
-        callback();
-      }
-    });
-  }
-
-  unregisterComponent(targetId: string): void {
-    const component = this.componentMap[targetId];
-    component.eventSubscription.unsubscribe();
-    for (const eventId in component) {
-      if (this.eventMap[eventId] === targetId) {
-        this.unregisterEvent(eventId);
-      }
-    }
-    delete this.componentMap[targetId];
+  subscribeToEvent = (eventId: string): Subscription => {
+    return this.eventMap[eventId].asObservable();
   }
 
   unregisterEvent(eventId: string): void {
